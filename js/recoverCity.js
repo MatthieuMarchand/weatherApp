529f15c543c79eff64a53ec981e25e00839acbed
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-05-30 12:00:42 +0200

    input city

--- js/recoverCity.js
+++ js/recoverCity.js
@@ -1,27 +1,9 @@
-// Fonction pour traiter la soumission du formulaire
 function handleFormSubmit(event) {
     event.preventDefault(); // Empêche le rechargement de la page par défaut
-
-    const city = document.getElementById('city').value;
-
-    const formData = new FormData(); // Crée un objet FormData
-    formData.append('city', city); // Ajoute la valeur de la zone de texte à FormData
-
-    fetch('pages/looking.html', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => {
-      // Gérer la réponse de la nouvelle page ici
-      console.error('Ville :', city); // ! a changer
-    })
-    .catch(error => {
-        console.error('Erreur lors de la soumission du formulaire', error);
-    });
+    const currentCity = document.getElementById('currentCity').value;
+    localStorage.setItem('currentCity', currentCity);
+    window.location.href = 'pages/looking.html';
 }
 
-// Récupère le formulaire de l'utilisateur
 const userForm = document.getElementById('userForm');
-
-// Ajoute un événement de soumission de formulaire
-userForm.addEventListener('submit', handleFormSubmit);
+userForm.addEventListener('submit', handleFormSubmit); // événement de soumission de formulaire
